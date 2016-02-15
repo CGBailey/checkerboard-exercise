cbae1c2a7d9bfb966ac9963c3b81ba77760ee7cf
Conrad G Bailey <grhmbailey@gmail.com>
2016-02-15 15:48:19 -0700

    checkerboard 1 done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+for (var i = 0; i < 3000; i++) {
+  var body = document.querySelector('body');
+  body.style.marginLeft = '-4px';
+  body.style.marginRight = '-4px';
+  body.style.marginTop = '-4px';
+  body.style.marginBottom = '-4px';
+
+  var redCheck = document.createElement('div');
+  redCheck.style.width = '11.1%';
+  redCheck.style.float = 'left';
+  redCheck.style.paddingBottom = '11.1%';
+  redCheck.style.backgroundColor = 'red';
+
+  var blackCheck = document.createElement('div');
+  blackCheck.style.width = '11.1%';
+  blackCheck.style.float = 'left';
+  blackCheck.style.paddingBottom = '11.1%';
+  blackCheck.style.backgroundColor = 'black';
+
+  if (i % 2 == 0) {
+    body.appendChild(blackBox);
+  } else {
+    body.appendChild(redBox);
+  }
+};
